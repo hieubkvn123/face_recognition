d1fa5910ae083a183e1f97f26dba9cef4406c047
Nong Minh Hieu <hieubkvn123@gmail.com>
2021-02-11 10:47:50 +0800

    Added mask detector weights file

--- mask_utils.py
+++ mask_utils.py
@@ -3,14 +3,37 @@ import cv2
 import numpy as np
 
 from tensorflow.keras.models import load_model
+from tensorflow.keras.models import Model
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input 
+from tensorflow.keras.applications import MobileNetV2 
+from tensorflow.keras.layers import *
 
 class MaskDetector(object):
-	def __init__(self, model_file='mask_detector.model'):
+	def __init__(self, model_file='mask_detector.model', weights_file='mask_detector.weights.hdf5'):
 		self.base_path = os.path.dirname(os.path.realpath(__file__))
 		self.model_file = os.path.join(self.base_path, model_file) 
-		self.mask_detector = load_model(self.model_file)
+		self.weights_file = os.path.join(self.base_path, weights_file)
+
+		try:
+			print('[INFO] Loading mask detector model from model file ... ')
+			self.mask_detector = load_model(self.model_file)
+		except:
+			print('[INFO] Loading mask detector model from weights file ... ')
+			self.mask_detector = self._model_arch()
+			self.mask_detector.load_weights(self.weights_file)
+
+	def _model_arch(self):
+		baseModel = MobileNetV2(weights='imagenet', include_top=False, input_tensor=Input(shape=(224,224,3)))
+		headModel = baseModel.output 
+		headModel = AveragePooling2D(pool_size=(7,7))(headModel)
+		headModel = Flatten()(headModel)
+		headModel = Dense(128, activation='relu')(headModel)
+		headModel = Dropout(0.5)(headModel)
+		headModel = Dense(2, activation='softmax')(headModel)
+
+		model = Model(inputs=baseModel.input, outputs=headModel)
+		return model
 
 	def _preprocess(self, image):
 		'''
